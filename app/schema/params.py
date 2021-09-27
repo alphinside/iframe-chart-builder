8df31c7e8cc2c1244fe0df7f6db1694586fc31c0
Alvin Prayuda <alvin.prayuda@pinhome.id>
2021-09-27 08:04:16 +0700

    feat: added validation for duplicate filters column

--- app/schema/params.py
+++ app/schema/params.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from typing import Any, List, Optional, Union
 
 from pydantic import BaseModel, validator
@@ -31,9 +32,19 @@ class BaseChartParams(BaseModel):
     filters: List[ColumnFilter] = []
 
     @validator("filters")
-    def limit_number_of_filters(cls, v):
+    def validate_and_limit_filters(cls, v):
         if len(v) > MAX_NUMBER_FILTERS:
             v = v[:MAX_NUMBER_FILTERS]
+
+        declared_cols = Counter([col_filter.column for col_filter in v])
+        duplicate_cols = [
+            column for column, count in declared_cols.items() if count > 1
+        ]
+
+        if len(duplicate_cols) != 0:
+            raise ValueError(
+                f"filter contain duplicate columns {duplicate_cols}"
+            )
 
         return v
 
