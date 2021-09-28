69c2cee77f963a50bc90fdb9d085fb1ca1803970
Alvin Prayuda <alvinprayuda@gmail.com>
2021-09-29 06:40:13 +0700

    refactor: refactor style param names and add filters entity styling (#15)
    * feat: added control entity styling
    
    * fix: wrong div placement
    
    Co-authored-by: Alvin Prayuda <alvin.prayuda@pinhome.id>

--- app/services/dash_layout/controls.py
+++ app/services/dash_layout/controls.py
@@ -16,75 +16,82 @@ from app.schema.requests import StyleDict
 
 
 def create_filters_control(
-    df: pd.DataFrame, filters: List[ColumnFilter], style: StyleDict
+    df: pd.DataFrame,
+    filters: List[ColumnFilter],
+    group_style: StyleDict = {},
+    entity_style: StyleDict = {},
 ):
     created_filters = []
 
     for column_filter in filters:
         if column_filter.type == DataTypes.categorical:
             cat_filter = create_categorical_filter(
-                df=df, column_filter=column_filter
+                df=df, column_filter=column_filter, entity_style=entity_style
             )
             created_filters.append(cat_filter)
         elif column_filter.type == DataTypes.numerical:
             num_filter = create_numerical_filter(
-                df=df, column_filter=column_filter
+                df=df, column_filter=column_filter, entity_style=entity_style
             )
             created_filters.append(num_filter)
         else:
             raise Exception("Unknown filter creation error")
 
-    return html.Div(created_filters, style=style)
+    if group_style is None:
+        group_style = {}
+
+    return html.Div(created_filters, style=group_style)
 
 
-def create_categorical_filter(df: pd.DataFrame, column_filter: ColumnFilter):
+def create_categorical_filter(
+    df: pd.DataFrame, column_filter: ColumnFilter, entity_style: StyleDict
+):
     categorical_selection = [
         {"label": k, "value": k}
         for k in sorted(df[column_filter.column].unique())
     ]
 
+    if entity_style is None:
+        entity_style = {}
+
     dropdown = html.Div(
         [
             dcc.Markdown(f"**{column_filter.column.title()}**"),
-            html.Div(
-                [
-                    dcc.Dropdown(
-                        id={
-                            "index": column_filter.column,
-                            "type": COLUMN_FILTER_CAT,
-                        },
-                        multi=True,
-                        options=categorical_selection,
-                        searchable=True,
-                    )
-                ]
+            dcc.Dropdown(
+                id={
+                    "index": column_filter.column,
+                    "type": COLUMN_FILTER_CAT,
+                },
+                multi=True,
+                options=categorical_selection,
+                searchable=True,
             ),
-            html.Div(
-                [
-                    dcc.Checklist(
-                        id={
-                            "index": column_filter.column,
-                            "type": COLUMN_FILTER_SELECT_ALL,
-                        },
-                        options=[
-                            {"label": "Select All", "value": SELECT_ALL_VALUE}
-                        ],
-                    )
-                ]
+            dcc.Checklist(
+                id={
+                    "index": column_filter.column,
+                    "type": COLUMN_FILTER_SELECT_ALL,
+                },
+                options=[{"label": "Select All", "value": SELECT_ALL_VALUE}],
             ),
         ],
+        style=entity_style,
     )
 
     return dropdown
 
 
-def create_numerical_filter(df: pd.DataFrame, column_filter: ColumnFilter):
+def create_numerical_filter(
+    df: pd.DataFrame, column_filter: ColumnFilter, entity_style: StyleDict
+):
     min_val, max_val = (
         df[column_filter.column].min(),
         df[column_filter.column].max(),
     )
     formatted_min = "{:0,.2f}".format(min_val)
     formatted_max = "{:0,.2f}".format(max_val)
+
+    if entity_style is None:
+        entity_style = {}
 
     input_fields = html.Div(
         [
@@ -136,7 +143,8 @@ def create_numerical_filter(df: pd.DataFrame, column_filter: ColumnFilter):
                 ],
                 style={"display": "flex"},
             ),
-        ]
+        ],
+        style=entity_style,
     )
 
     return input_fields
